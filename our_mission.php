a6f29c335eef7e8a5a63474f2c9347188f8b3408
Matt MacNeil <matt@mattmacneil.com>
2017-04-24 19:07:01 -0400

    Removed unused subhero attempts.

--- our_mission.php
+++ our_mission.php
@@ -9,7 +9,7 @@
 
 	<div class="u-Section-wrap">
 
-		<section class="Subpage-hero u-Section-Pad--subhero t-center">
+		<section class="Subpage-hero u-Section-Pad--subhero">
 
 			<span class="headliner u-Color-darkblue" ><span class="u-Color-lightblue">Our mission &ndash;<br /></span>see what we care about.</span>
 
@@ -19,54 +19,39 @@
 
 </div><!-- subpage bg -->
 
-hi
-
-<div style="width: 100%; height: 400px;"></div>
-
 <div class="u-Section-wrap">
 
-<section class="Landing-culture">
+<section class="UnderHero u-Section-Pad">
 
-	<div class="Landing-culture-overlay">
+	<div class="Layout-left">
 
-		<div class="Landing-culture-left u-Section-Pad u-Color-white">
-
-			<h1 class="headliner">Our culture makes us unique</h1>
-
-			<p class="t-dinregular">Imagine a travel experience free of bag tags, tickets, lines and instead a connected world that anticipates customer needs, powered by the Internet of Things, artificial intelligence, big data and predictive analytics.</p>
-
-		</div><!-- landing culture left -->
-
-	</div><!-- landing culture overlay -->
-
-</section><!-- landing culture -->
-
-<section class="UnderHero u-Section-Pad Landing-map">
-
-	<div class="Layout-left Landing-map-left">
-
-		<h3 class="headliner u-Color-darkblue u-marbottom-50">And we&#8217;re in the heart of it all.</h3>
-
-		<p>For an airline with crewmembers in over 100 destinations, our office is pretty easy to find. We’ve tapped into Silicon Valley and are proud to call the region home</p>
-
-		<div class="Landing-map-address">
-
-			<span class="t-dinregular">JetBlue Technology Ventures</span><br />
-			<span>666 Skyway Rd, Suite 420</span><br />
-			<span>San Carlos, CA 94069</span>
-
-		</div><!-- landing map address -->
+		<h2 class="headliner u-Color-darkblue">We're focused on the future:</h2>
 
 	</div><!-- layout left -->
 
-	<div class="Layout-right Landing-map-right">
+	<div class="Layout-right u-Section-Pad--side">
 
-		<div class="u-Fill-lightgrey Landing-map-wrapper">
+		<p>JetBlue is a company known for award-winning customer service, delivered by helpful and friendly crewmembers. Future technologies to power seamless customer and crewmember interactions across the entire digital and physical travel experience include personalization, geolocation, customization, messaging, virtual reality, improved tools for our crewmembers to deliver magnificent service or enhanced ways to extend the JetBlue brand and culture.</p>
 
-			<div id="Landing-map-gmaps">
-			</div><!-- landing map gmaps -->
+		<p>Imagine a travel experience free of bag tags, tickets, lines and instead a connected world that anticipates customer needs, powered by the Internet of Things, artificial intelligence, big data and predictive analytics.</p>
 
-		</div><!-- landing map -->
+		<div class="Landing-portfolio-container">
+
+			<a href="http://flyrlabs.com/" target="_blank"><img src="images/Landing-portfolio-flyr.png" alt="Go to www.flyrlabs.com" /></a>
+
+			<a href="https://filament.com/" target="_blank"><img src="images/Landing-portfolio-filament.png" alt="Go to www.filament.com" /></a>
+
+			<a href="http://mozio.com/" target="_blank"><img src="images/Landing-portfolio-mozio.png" alt="Go to www.mozio.com" /></a>
+
+		</div><!-- landing portfolio container -->
+
+		<div class="Landing-portfolio-container">
+
+			<a href="http://zunum.aero/" target="_blank"><img src="images/Landing-portfolio-zunum.png" alt="Go to www.zunum.aero" /></a>
+
+			<a href="https://www.30secondstofly.com/" target="_blank"><img src="images/Landing-portfolio-claire.png" alt="Go to www.30secondstofly.com" /></a>
+
+		</div><!-- landing portfolio container -->
 
 	</div><!-- layout right -->
 
@@ -74,7 +59,7 @@ hi
 
 </section><!-- underhero -->
 
-</div><!-- section wrap (culture / map) -->
+</div><!-- section wrap (mission fields) -->
 
 <?php 
 
