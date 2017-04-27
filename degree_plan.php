a6ba7f95d84710d6dbecb24e844f2616a4c40739
Matt MacNeil <matt@mattmacneil.com>
2017-04-27 18:29:59 -0400

    Added more content to degree plan.

--- degree_plan.php
+++ degree_plan.php
@@ -2,55 +2,88 @@
 
 <?php
     include "head.php";
+    include "nav.php";
 ?>
 
 <style type="text/css">
 
 .classlist {
-	padding: 25px 10%;
+	padding: 0 5% 100px 5%;
 	color: #000;
 }	
 
 	.classlist .headliner {
-		font-size: 2em;
+		font-size: 1.5em;
 		display: inline-block;
 		margin: 5px 0 25px 0;
 	}
 
-	li {
-		margin-bottom: 20px;
+	.classlist li {
+		margin-bottom: 40px;
+	}
+
+	.classlist .classcategory {
+		font-family: "dinbold";
+	}
+
+	.classlist .classname {
+		font-family: "dinbold";
+		color: #75778f;
+		display: inline-block;
+		margin-bottom: 15px;
+	}
+
+	.classlist p {
+		padding: 0 10%;
 	}
 
 </style>
 
 <body class="classlist">
 
+<div style="height: 100px;"></div>
+
 <span class="headliner">Intellectual and Practical Skills</span><br />
 
 	<ul>
 		<li>
-			1. English Composition I	<br />
-			EN1103-English Composition<br />
+			<span class="classcategory">1. English Composition I</span><br />
+			<span class="classname">EN1103-English Composition</span><br />
+			<p>
+				Write / edit crewmember quotes to be featured in each section.
+			</p>
 		</li>
 
 		<li>
-			2. English Composition II	<br />
-			ENG1111-College Wrt 2<br />
+			<span class="classcategory">2. English Composition II</span><br />
+			<span class="classname">ENG1111-College Wrt 2</span><br />
+			<p>
+				Apply skills to write long-form descriptions for each "what we care about" subsection.
+			</p>
 		</li>
 
 		<li>
-			3. Oral Communication	<br />
-			Introduction to Communication (SL)<br />
+			<span class="classcategory">3. Oral Communication</span><br />
+			<span class="classname">Introduction to Communication (SL)</span><br />
+			<p>
+				Describe audience(s) for site and how it applies to site.
+			</p>
 		</li>
 
 		<li>
-			4. Quantitative Literacy<br />	
-			MTH1192- Pre Calc<br />
+			<span class="classcategory">4. Quantitative Literacy</span><br />	
+			<span class="classname">MTH1192- Pre Calc</span><br />
+			<p>
+				Describe grid system used to build site, integrate resposive breakpoint explaination.
+			</p>
 		</li>
 
 		<li>
-			5. Information Literacy	<br />
-			Technical Writing (TECEP)<br />
+			<span class="classcategory">5. Information Literacy</span><br />
+			<span class="classname">Technical Writing (TECEP)</span><br />
+			<p>
+				Describe process of getting to office, per TECEP instructional writing guidelines.
+			</p>
 		</li>
 	</ul>
 
@@ -59,129 +92,322 @@
 	<ul>
 
 		<li>
-			1. Diversity	<br />
-			Intro to Sociology (SC)<br />
+			<span class="classcategory">1. Diversity</span><br />
+			<span class="classname">Intro to Sociology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
 		</li>
 
 		<li>
-			2. Ethics	<br />
-			Business Ethics (SL)<br />
+			<span class="classcategory">2. Ethics</span><br />
+			<span class="classname">Business Ethics (SL)</span><br />
+			<p>
+				Hello.
+			</p>
 		</li>
 
 		<li>
-			3. Civic Engagement	<br />
-			American Government (SC)<br />
+			<span class="classcategory">3. Civic Engagement</span><br />
+			<span class="classname">American Government (SC)</span><br />
+			<p>
+				Hello.
+			</p>
 		</li>
 
 	</ul>
 
 <span class="headliner">Knowledge of Human Cultures</span><br />
+	
+	<ul>
 
-	1. Human Cultures Course	<br />
-	The Civil War and Reconstruction (SC)<br />
+		<li>
+			<span class="classcategory">1. Human Cultures Course</span><br />
+			<span class="classname">The Civil War and Reconstruction (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	2. Human Cultures Course	<br />
-	US History I (SC)<br />
+		<li>
+			<span class="classcategory">2. Human Cultures Course</span><br />
+			<span class="classname">US History I (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	3. Human Cultures Course	<br />
-	Intro to Psychology (SC)<br />
+		<li>
+			<span class="classcategory">3. Human Cultures Course</span><br />
+			<span class="classname">Intro to Psychology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	4. Human Cultures Course<br />
-	COURSE OPTIONS	<br />
+		<li>
+			<span class="classcategory">4. Human Cultures Course</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+	</ul>
 
 <span class="headliner">Understanding the Physical and Natural World</span><br />
 
-	1. Natural World Course	<br />
-	Computer Concepts and Applications<br />
+	<ul>
 
-	2. Natural World Course<br />
-	Intro to Computing (SC)<br />
+		<li>
+			<span class="classcategory">1. Natural World Course</span><br />
+			<span class="classname">Computer Concepts and Applications</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">2. Natural World Course</span><br />
+			<span class="classname">Intro to Computing (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+	</ul>
 
 <span class="headliner">General Education Electives</span><br />
 
-	1. General Education Elective	<br />
-	Human Growth and Development (SC)<br />
+	<ul>
+	
+		<li>
+			<span class="classcategory">1. General Education Elective</span><br />
+			<span class="classname">Human Growth and Development (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	2. General Education Elective	<br />
-	COURSE OPTIONS		<br />
+		<li>
+			<span class="classcategory">2. General Education Elective</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	3. General Education Elective	<br />
-	COURSE OPTIONS		<br />
+		<li>
+			<span class="classcategory">3. General Education Elective	</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	4. General Education Elective	<br />
-	COURSE OPTIONS		<br />
+		<li>
+			<span class="classcategory">4. General Education Elective	</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	5. General Education Elective	<br />
-	COURSE OPTIONS		<br />
+		<li>
+			<span class="classcategory">5. General Education Elective	</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	6. General Education Elective	<br />
-	COURSE OPTIONS		<br />
+		<li>
+			<span class="classcategory">6. General Education Elective	</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	7. General Education Elective <br />
-	COURSE OPTIONS	<br />
+		<li>
+			<span class="classcategory">7. General Education Elective </span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-<span class="headliner">Area of Study Upper Division</span><br />
+	</ul>
 
-	1. Upper Division	<br />
-	Social Psychology (SC)<br />
+<span class="headliner">Area of Study (Upper Division)</span><br />
 
-	2. Upper Division	<br />
-	Abnormal Psychology (SC)<br />
+	<ul>
 
-	3. Upper Division	<br />
-	Research Methods in Psychology (SC)<br />
+		<li>
+			<span class="classcategory">1. Upper Division</span><br />
+			<span class="classname">Social Psychology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	4. Upper Division	<br />
-	History of the Vietnam War (SC)<br />
+		<li>
+			<span class="classcategory">2. Upper Division</span><br />
+			<span class="classname">Abnormal Psychology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	5. Upper Division	<br />
-	Organizational Behavior (SL)<br />
+		<li>
+			<span class="classcategory">3. Upper Division</span><br />
+			<span class="classname">Research Methods in Psychology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-<span class="headliner">Area of Study Lower Division</span><br />
+		<li>
+			<span class="classcategory">4. Upper Division</span><br />
+			<span class="classname">History of the Vietnam War (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	1. Lower Division	<br />
-	Educational Psychology (SC)<br />
+		<li>
+			<span class="classcategory">5. Upper Division</span><br />
+			<span class="classname">Organizational Behavior (SL)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	2. Lower Division	<br />
-	HST4559-Come to America<br />
+	</ul>
 
-	3. Lower Division	<br />
-	ECN1115-Prin Macroec<br />
+<span class="headliner">Area of Study (Lower Division)</span><br />
 
-	4. Lower Division 100	<br />
-	Visual Communications (SO)<br />
+	<ul>
 
-	5. Lower Division 100	<br />
-	Introduction to Art History (SO)<br />
+		<li>
+			<span class="classcategory">1. Lower Division</span><br />
+			<span class="classname">Educational Psychology (SC)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">2. Lower Division</span><br />
+			<span class="classname">HST4559-Come to America</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">3. Lower Division</span><br />
+			<span class="classname">ECN1115-Prin Macroec</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">4. Lower Division 100</span><br />
+			<span class="classname">Visual Communications (SO)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">5. Lower Division 100</span><br />
+			<span class="classname">Introduction to Art History (SO)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+	</ul>
 
 <span class="headliner">Free Electives</span><br />
 
-	1. Free Elective	<br />
-	Project Management (SO)<br />
+	<ul>
 
-	2. Free Elective	<br />
-	BL4101-Bus Law 1<br />
+		<li>
+			<span class="classcategory">1. Free Elective</span><br />
+			<span class="classname">Project Management (SO)</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	3. Free Elective	<br />
-	IT4236-PC Db Sftwr<br />
+		<li>
+			<span class="classcategory">2. Free Elective</span><br />
+			<span class="classname">BL4101-Bus Law 1</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	4. Free Elective	<br />
-	BU6203-Intro to Web Publ<br />ishing
+		<li>
+			<span class="classcategory">3. Free Elective</span><br />
+			<span class="classname">IT4236-PC Db Sftwr</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	5. Free Elective	<br />
-	GET1170-Eng Graph 1<br />
+		<li>
+			<span class="classcategory">4. Free Elective</span><br />
+			<span class="classname">BU6203-Intro to Web Publishing</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	6. Free Elective	<br />
-	BU2104- Financial Accoun<br />ting
+		<li>
+			<span class="classcategory">5. Free Elective</span><br />
+			<span class="classname">GET1170-Eng Graph 1</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 
-	7. Free Elective	<br />
-	COURSE OPTIONS<br />
+		<li>
+			<span class="classcategory">6. Free Elective</span><br />
+			<span class="classname">BU2104- Financial Accounting</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+		<li>
+			<span class="classcategory">7. Free Elective</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 	
-	8. Free Elective	<br />
-	COURSE OPTIONS<br />
+		<li>
+			<span class="classcategory">8. Free Elective</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
 	
-	9. Free Elective	<br />
-	COURSE OPTIONS
+		<li>
+			<span class="classcategory">9. Free Elective</span><br />
+			<span class="classname">COURSE OPTIONS</span><br />
+			<p>
+				Hello.
+			</p>
+		</li>
+
+	</ul>
 
 </body>
 <html>
