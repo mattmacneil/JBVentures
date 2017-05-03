7aef0c42e1a155b7eb6bd048605bd99329b43071
Matt MacNeil <matt@mattmacneil.com>
2017-05-03 15:44:13 -0400

    Changed landing hero background image and underlying texture.

---
+++ oldhero.php
@@ -0,0 +1,82 @@
+
+<div style="height: 300px;"></div>
+
+<div class="u-Fill-green divshadow">
+
+<div class="u-Section-wrap">
+
+<section class="Hero Hero-landing">
+
+	<div class="Hero-video">
+
+		<div class="Hero-video-wrapper">
+
+			<video autoplay muted loop>
+	    	<source src="images/Hero-landing.webm" type="video/webm">
+	    	<source src="images/Hero-landing.mp4" type="video/mp4">
+	    	<source src="images/Hero-landing.ogv" type="video/ogv">
+			</video>
+
+		</div><!-- hero video wrapper -->
+
+		<div class="Hero-overlay">
+
+			<div class="Hero-left u-Color-white greenhaze">
+
+				<h1 class="headliner">We&#8217;ve<br /> taken off!</h1>
+
+				<p class="t-dinregular">JetBlue Technology Ventures incubates, invests in and partners with early stage startups at the intersection of technology, travel, and hospitality.</p>
+
+			</div><!-- hero left -->
+
+			<div class="Hero-right">
+
+				<div class="Hero-right-inner t-center">
+
+					<div class="Hero-landing-image-wrap">
+
+						<div class="Hero-hex-1"></div><!-- hero hex 1 -->
+						<div class="Hero-hex-2"></div><!-- hero hex 2 -->
+						<div class="Hero-hex-3"></div><!-- hero hex 3 -->
+
+						<div class="Hero-hex-icon-1"></div><!-- hero hex icon 1 -->
+						<div class="Hero-hex-icon-2"></div><!-- hero hex icon 2 -->
+						<div class="Hero-hex-icon-3"></div><!-- hero hex icon 3 -->
+
+						<div class="Hero-hex-dot-1">
+							<div class="Hero-hex-dot-inner">
+							</div><!-- hero hex arrow inner -->
+						</div><!-- hero hex arrow 1 -->
+
+						<div class="Hero-hex-dot-2">
+							<div class="Hero-hex-dot-inner">
+							</div><!-- hero hex arrow inner -->
+						</div><!-- hero hex arrow 2 -->
+
+						<div class="Hero-hex-dot-3">
+							<div class="Hero-hex-dot-inner">
+							</div><!-- hero hex arrow inner -->
+						</div><!-- hero hex arrow 3 -->
+
+						<div class="Hero-hex-logo"></div><!-- hero hex logo -->
+
+					</div><!-- hero landing image wrap -->
+
+				</div><!-- hero right inner -->
+
+				<div class="Hero-right-widescreen-gutter">
+				</div><!-- hero right widescreen gutter -->
+
+			</div><!-- hero right -->
+
+			<div class="clearfix"></div>
+
+		</div><!-- landing hero overlay -->
+
+	</div><!-- hero video -->
+
+</section><!-- landing hero -->
+
+</div><!-- section wrap (hero) -->
+
+</div><!-- Hero bg -->
