4a3cb89b0b4238d864a99c02bbc0e6374c1a941c
Matt MacNeil <matt@mattmacneil.com>
2017-05-15 15:12:48 -0400

    Removed some whitespace in index file.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     include "head.php";
     include "nav.php";
 ?>
-
 <div class="Landing-Hero-texture">
 	<div class="Landing-Hero_movement">
 		<div class="Landing-Hero-overlay">
@@ -52,118 +51,63 @@
 		</div><!-- landing hero overlay -->
 	</div><!-- landing hero movement -->
 </div><!-- landing hero texture -->
-
 <div class="u-Section-wrap">
-
 <section class="UnderHero u-Section-Pad UnderClouds">
-
 	<div class="Layout-left">
-
 		<h2 class="headliner u-Color-darkblue">Our portfolio is pretty fly&hellip;</h2>
-
 	</div><!-- layout left -->
-
 	<div class="Layout-right u-Section-Pad--side">
-
 		<p>JetBlue is a company known for award-winning customer service, delivered by helpful and friendly crewmembers. Future technologies to power seamless customer and crewmember interactions across the entire digital and physical travel experience include personalization, geolocation, customization, messaging, virtual reality, improved tools for our crewmembers to deliver magnificent service or enhanced ways to extend the JetBlue brand and culture.</p>
-
 		<p>Imagine a travel experience free of bag tags, tickets, lines and instead a connected world that anticipates customer needs, powered by the Internet of Things, artificial intelligence, big data and predictive analytics.</p>
-
 		<div class="Landing-portfolio-container">
-
 			<a href="http://flyrlabs.com/" target="_blank"><img src="images/Landing-portfolio-flyr.png" alt="Go to www.flyrlabs.com" /></a>
-
 			<a href="https://filament.com/" target="_blank"><img src="images/Landing-portfolio-filament.png" alt="Go to www.filament.com" /></a>
-
 			<a href="http://mozio.com/" target="_blank"><img src="images/Landing-portfolio-mozio.png" alt="Go to www.mozio.com" /></a>
-
 		</div><!-- landing portfolio container -->
-
 		<div class="Landing-portfolio-container">
-
 			<a href="http://zunum.aero/" target="_blank"><img src="images/Landing-portfolio-zunum.png" alt="Go to www.zunum.aero" /></a>
-
 			<a href="https://www.30secondstofly.com/" target="_blank"><img src="images/Landing-portfolio-claire.png" alt="Go to www.30secondstofly.com" /></a>
-
 		</div><!-- landing portfolio container -->
-
 	</div><!-- layout right -->
-
 	<div class="clearfix"></div>
-
 </section><!-- underhero -->
-
 </div><!-- section wrap (landing portfolio) -->
-
 <div class="u-Fill-darkblue">
-
 	<div class="u-Section-wrap">
-
 		<section class="Landing-culture">
-
 				<div class="Landing-culture-gutter-left">
 				</div><!-- landing culture gutter left -->
-
 				<div class="Landing-culture-gutter-right">
 				</div><!-- landing culture gutter right -->
-
 			<div class="Landing-culture-overlay">
-
 				<div class="Landing-culture-left u-Section-Pad u-Color-white">
-
 					<h1 class="headliner">our culture makes us unique&hellip;</h1>
-
 					<p class="t-dinregular">Imagine a travel experience free of bag tags, tickets, lines and instead a connected world that anticipates customer needs, powered by the Internet of Things, artificial intelligence, big data and predictive analytics.</p>
-
 				</div><!-- landing culture left -->
-
 			</div><!-- landing culture overlay -->
-
 		</section><!-- landing culture -->
-
 	</div><!-- section wrap (culture) -->
-
 </div><!-- fill (culture) -->
-
 <div class="u-Section-wrap">
-
 <section class="UnderHero u-Section-Pad Landing-map">
-
 	<div class="Layout-left Landing-map-left">
-
 		<h3 class="headliner u-Color-darkblue u-marbottom-50">and we&#8217;re in the heart of it all!</h3>
-
 		<p>For an airline with crewmembers in over 100 destinations, our office is pretty easy to find. We’ve tapped into Silicon Valley and are proud to call the region home</p>
-
 		<div class="Landing-map-address">
-
 			<span class="t-dinregular">JetBlue Technology Ventures</span><br />
 			<span>666 Skyway Rd, Suite 420</span><br />
 			<span>San Carlos, CA 94069</span>
-
 		</div><!-- landing map address -->
-
 	</div><!-- layout left -->
-
 	<div class="Layout-right Landing-map-right">
-
 		<div class="u-Fill-lightgrey Landing-map-wrapper">
-
 			<div id="Landing-map-gmaps">
 			</div><!-- landing map gmaps -->
-
 		</div><!-- landing map -->
-
 	</div><!-- layout right -->
-
 	<div class="clearfix"></div>
-
 </section><!-- underhero -->
-
 </div><!-- section wrap (culture / map) -->
-
 <?php 
-
     include "footer.php";
-
 ?>
